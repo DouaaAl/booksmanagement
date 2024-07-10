e06d979a0972576b6b8ee995396de3f889643841
DouaaAl <elmahraouidouaa123@gmail.com>
2024-07-10 14:03:28 +0100

    fixed path again

--- server/routers/bill.js
+++ server/routers/bill.js
@@ -96,7 +96,7 @@ router.post("/report/view", verify, async (req, res) => {
                     }
 
                     try {
-                        const pdfResult = pdf.create(data).toFile(__dirname,`../generated_pdf/${generateUuid}.pdf`, (err, pdfResult)=>{
+                        const pdfResult = pdf.create(data).toFile(__dirname,`generated_pdf/${generateUuid}.pdf`, (err, pdfResult)=>{
                             console.log("pdf created successfully");
                             res.status(200).json({ message: generateUuid });             
                         });
@@ -118,9 +118,7 @@ router.post("/report/view", verify, async (req, res) => {
 
 
 router.get('/pdf/get/:id', (req, res) => {
-    console.log("dirname");
     const pdfPath = path.join(__dirname, '../generated_pdf', req.params.id + ".pdf");
-    console.log("this is pdf path :",pdfPath);
     fs.readFile(pdfPath, (err, data) => {
       if (err) {
         console.error('Error reading PDF file:', err);
